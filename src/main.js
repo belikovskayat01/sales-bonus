e5fba2524db26a194bc9f2b29288c334afedcd6d
Tatiana Belikova <belikovaat@yandex.ru>
2026-06-17 18:33:17 +0300

    Код до Этап 3: Шаг 2

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@
  */
 function calculateSimpleRevenue(purchase, _product) {
    // @TODO: Расчет выручки от операции
+   const { discount, sale_price, quantity } = purchase;
+
+   const discountFactor = 1 - (discount / 100);
+
+   return sale_price * quantity * discountFactor;
 }
 
 /**
@@ -17,6 +22,7 @@ function calculateSimpleRevenue(purchase, _product) {
  */
 function calculateBonusByProfit(index, total, seller) {
     // @TODO: Расчет бонуса от позиции в рейтинге
+    const { profit} = seller;
 }
 
 /**
@@ -27,14 +33,72 @@ function calculateBonusByProfit(index, total, seller) {
  */
 function analyzeSalesData(data, options) {
     // @TODO: Проверка входных данных
-
+    if (!data
+        || !Array.isArray(data.sellers)
+        || data.sellers.length === 0
+        || !Array.isArray(data.products)
+        || data.products.length === 0
+        || !Array.isArray(data.purchase_records)
+        || data.purchase_records.length === 0
+    ) {
+        throw new Error('Неверные входные данные');
+    }
     // @TODO: Проверка наличия опций
+    if (!options || typeof options !== 'object') {
+        throw new Error('Опции отсутствуют');
+    }
 
+    const { calculateRevenue,calculateBonus } = options;
+
+    if(!calculateRevenue || !calculateBonus) {
+        throw new Error('Отсутствуют необходимые для расчета функции');
+    }
+
+    if (typeof calculateRevenue !== 'function' || typeof calculateBonus !== 'function') {
+        throw new Error('Переданные опции не являются функциями');
+    }
     // @TODO: Подготовка промежуточных данных для сбора статистики
-
+    const sellerStats = data.sellers.map(seller => ({
+        id: seller.id,
+        name: `${seller.first_name} ${seller.last_name}`,
+        revenue: 0,
+        profit: 0,
+        sales_count: 0,
+      products_sold: {}
+    }));
     // @TODO: Индексация продавцов и товаров для быстрого доступа
+    const sellerIndex = Object.fromEntries(
+        sellerStats.map(seller => [seller.id, seller])
+    );
 
+    const productIndex = Object.fromEntries(
+        data.products.map(product => [product.sku, product])
+    );
     // @TODO: Расчет выручки и прибыли для каждого продавца
+    data.purchase_records.forEach(record => {
+        const seller = sellerIndex[record.seller_id];
+
+        seller.sales_count +=1;
+
+        let totalRevenue = 0;
+
+        record.items.forEach(item => {
+            const product = productIndex[item.sku];
+            const revenue = calculateRevenue(item, product);
+            const cost = product.purchase_price * item.quantity;
+            const profit = revenue - cost;
+
+            seller.profit += profit;
+            totalRevenue += revenue;
+
+            if (!seller.products_sold[item.sku]) {
+                seller.products_sold[item.sku] = 0;
+            }
+            seller.products_sold[item.sku] += item.quantity;
+        });
+
+        seller.revenue += totalRevenue;
+    });
 
     // @TODO: Сортировка продавцов по прибыли
 
